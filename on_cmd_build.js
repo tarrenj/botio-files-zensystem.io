fa3fcee6265853547e045202532f96f68bc5b72f
cronicc <cronicc@users.noreply.github.com>
2017-05-09 02:10:35 +0000

    production config

--- on_cmd_build.js
+++ on_cmd_build.js
@@ -1,16 +1,53 @@
 var botio = require(process.env['BOTIO_MODULE']);
 var shell = require('shelljs');
-var now = require("performance-now")
-var root_url = botio.public_url
+var now = require('performance-now');
+var cmd;
+var suppress = false;
+var runtime = true;
+var root_url = botio.public_url.slice(0, -16);
 
-root_url = root_url.slice(0, -16);
+// Run commdand synchronously, exit if error
+function runCommand(cmd, suppress, runtime) {
+    shell.echo('\nRunning command "' + cmd + '"\n');
+    var start = now();
+    var run = shell.exec(cmd, {silent:suppress});
+    var end = now();
+    if (runtime) {
+        shell.echo('\nCommand "' + cmd + '" finished after ' + (end - start) / 1000 + ' seconds\n')
+    }
+    if (run.code !== 0) {
+        shell.echo('\nError: Build failed at step "' + cmd + '" with ExitCode ' + run.code + '\n');
+        botio.message('Error: Build failed at step "' + cmd + '" with ExitCode ' + run.code);
+        shell.exit(1);
+    }
+}
 
-var a = now();
-shell.echo("Running the build chain\n");
 
-shell.exec('bundle install --path ./vendor/bundle && npm install && bower install && rm -rf _site/* && gulp sass concat jekyll-build && rm -rf /mnt/latest/* && cp -r ./_site/* /mnt/latest/ && tar -czf latest.tar.gz -C /mnt/latest . && cp ./latest.tar.gz /mnt/public');
-var b = now();
+cmd = 'bundle install --path ./vendor/bundle';
+runCommand(cmd, suppress, runtime);
 
-shell.echo("\nBuild chain finished: " + (b - a) / 1000 + " seconds\n");
+cmd = 'npm install';
+runCommand(cmd, suppress, runtime);
+
+cmd = 'bower install';
+runCommand(cmd, suppress, runtime);
+
+cmd = 'rm -rf _site/*';
+runCommand(cmd, suppress, runtime);
+
+cmd = 'gulp sass concat jekyll-build';
+runCommand(cmd, suppress, runtime);
+
+cmd = 'rm -rf /mnt/latest/*';
+runCommand(cmd, suppress, runtime);
+
+cmd = 'cp -r ./_site/* /mnt/latest/';
+runCommand(cmd, suppress, runtime);
+
+cmd = 'tar -czf latest.tar.gz -C /mnt/latest .';
+runCommand(cmd, suppress, runtime);
+
+cmd = 'cp ./latest.tar.gz /mnt/public';
+runCommand(cmd, suppress, runtime);
 
 botio.message('You can download the generated site at: '+root_url+'/latest.tar.gz');
--- on_cmd_preview.js
+++ on_cmd_preview.js
@@ -1,11 +1,30 @@
 var botio = require(process.env['BOTIO_MODULE']);
 var shell = require('shelljs');
-var root_url = botio.public_url
+var now = require('performance-now');
+var cmd;
+var suppress = false;
+var runtime = true;
+var root_url = botio.public_url.slice(0, -16);
 
-root_url = root_url.slice(0, -16);
+// Run commdand synchronously, exit if error
+function runCommand(cmd, suppress, runtime) {
+    shell.echo('\nRunning command "' + cmd + '"\n');
+    var start = now();
+    var run = shell.exec(cmd, {silent:suppress});
+    var end = now();
+    if (runtime) {
+        shell.echo('\nCommand "' + cmd + '" finished after ' + (end - start) / 1000 + ' seconds\n')
+    }
+    if (run.code !== 0) {
+        shell.echo('\nError: Build failed at step "' + cmd + '" with ExitCode ' + run.code + '\n');
+        botio.message('Error: Build failed at step "' + cmd + '" with ExitCode ' + run.code);
+        shell.exit(1);
+    }
+}
 
 // Recursively copy latest build to public directory
-shell.exec('cp -r /mnt/latest/* /mnt/public');
+cmd = 'cp -r /mnt/latest/* /mnt/public';
+runCommand(cmd, suppress, runtime);
 
 botio.message('#### Published');
-botio.message('You can view your generated site at: '+root_url);
+botio.message('You can view your repo files at: '+root_url);
